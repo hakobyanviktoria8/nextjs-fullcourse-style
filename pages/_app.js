e4585f69a888a81c1358f573d032a14a9436f99d
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-03-30 18:33:48 +0400

    add bootstrap link and work style.module.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+// all link added here see all pages
 import '../styles/globals.css'
+// add bootstrap link
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "../styles/About.module.css"
+
+function About() {
+  return (
+    <div>
+        <h1 className={styles.title}>About page</h1>
+        <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  )
+}
+
+export default About
